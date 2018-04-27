8cc1e070b5001a8d80140ddb3fe4487552dcf998
GrowZHax <testerping18@gmail.com>
2018-04-28 02:38:39 +0300

    Initial commit

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        public static int Base = 0x00509b74;
+        public static int Health = 0xF8;
+        public static int SniperAmmo = 0x14c;
+
+        static void Main(string[] args)
+        {
+            VAMemory vam = new VAMemory("ac_client");
+            Console.Write("Assault Cube Founded !");
+            int LocalPlayer = vam.ReadInt32((IntPtr)0x00509b74);
+            while (true)
+            {
+                int address = LocalPlayer + Health;
+                vam.WriteInt32((IntPtr)address, 12341);
+
+                address = LocalPlayer + SniperAmmo;
+                vam.WriteInt32((IntPtr)address, 12341);
+            }
+        }
+    }
+}
+    
